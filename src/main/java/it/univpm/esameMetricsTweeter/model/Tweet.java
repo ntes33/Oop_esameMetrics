8e96ce575fe803dd56d1d2dfa6b6c91fb1a8c903
Steve ngtsa <stevenguetsa@yahoo.fr>
2022-07-14 22:40:34 +0200

    updat model lombok

--- src/main/java/it/univpm/esameMetricsTweeter/model/Tweet.java
+++ src/main/java/it/univpm/esameMetricsTweeter/model/Tweet.java
@@ -5,10 +5,22 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+
+
+            //Classe che modella tutti gli attribuiti
+
+
+//@AllArgsConstructor
+ 
+@AllArgsConstructor
+@Data  
+
 public class Tweet {
-
-
-		
+	
 		    // Data di creazione del Tweet
 		    private String created_at;
 		    
@@ -48,152 +60,7 @@ public class Tweet {
 		    public Tweet() {
 		        
 		    }
-		    public Tweet(String created_at, long idtweet, String text, String source, Long n_like, Long n_retweet,
-		            boolean favourites, boolean retweeted, long id_user, String name, String location, Long follower,
-		            Long following) {
-		        super();
-		        this.created_at = created_at;
-		        this.idtweet = idtweet;
-		        this.text = text;
-		        this.source = source;
-		        this.n_like = n_like;
-		        this.n_retweet = n_retweet;
-		        this.favourites = favourites;
-		        this.retweeted = retweeted;
-		        this.id_user = id_user;
-		        this.name = name;
-		        this.Location = location;
-		        this.Follower = follower;
-		        this.Following = following;
-		    }
-		    public String getCreated_at() {
-		        return created_at;
-		    }
 
-
-		    public void setCreated_at(String created_at) {
-		        this.created_at = created_at;
-		    }
-
-
-		    public long getIdtweet() {
-		        return idtweet;
-		    }
-
-
-		    public void setIdtweet(long idtweet) {
-		        this.idtweet = idtweet;
-		    }
-
-
-		    public String getText() {
-		        return text;
-		    }
-
-
-		    public void setText(String text) {
-		        this.text = text;
-		    }
-
-
-		    public String getSource() {
-		        return source;
-		    }
-
-
-		    public void setSource(String source) {
-		        this.source = source;
-		    }
-		    public Long getN_like() {
-		        return n_like;
-		    }
-
-
-		    public void setN_like(Long n_like) {
-		        this.n_like = n_like;
-		    }
-
-
-		    public Long getN_retweet() {
-		        return n_retweet;
-		    }
-
-
-		    public void setN_retweet(Long n_retweet) {
-		        this.n_retweet = n_retweet;
-		    }
-
-
-		    public boolean isFavourites() {
-		        return favourites;
-		    }
-
-
-		    public void setFavourites(boolean favourites) {
-		        this.favourites = favourites;
-		    }
-
-
-		    public boolean isRetweeted() {
-		        return retweeted;
-		    }
-
-
-		    public void setRetweeted(boolean retweeted) {
-		        this.retweeted = retweeted;
-		    }
-		    public long getId_user() {
-		        return id_user;
-		    }
-
-
-		    public void setId_user(long id) {
-		        this.id_user = id;
-		    }
-
-
-		    public String getName() {
-		        return name;
-		    }
-
-
-		    public void setName(String name) {
-		        this.name = name;
-		    }
-
-
-		    public String getLocation() {
-		        return Location;
-		    }
-
-
-		    public void setLocation(String location) {
-		        Location = location;
-		    }
-
-
-		    public Long getFollower() {
-		        return Follower;
-		    }
-
-
-		    public void setFollower(Long follower) {
-		        Follower = follower;
-		    }
-
-
-		    public Long getFollowing() {
-		        return Following;
-		    }
-
-
-		    public void setFollowing(Long following) {
-		        Following = following;
-		    }
-		    
-		    
-			 
-		    
 		    /**
 			 * Fa parsing della data e trasforma la stringa in un oggetto Date
 			 *
@@ -205,9 +72,5 @@ public class Tweet {
 				sf.setLenient(false);
 				return sf.parse(this.created_at);
 			}
-		    
-	
-	
-	
-	
+
 }
